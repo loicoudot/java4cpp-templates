225cf3a5c8f8a6ebec0b1f49f63bacb5ddf0ed05
loic.oudot <loic.oudot@gmail.com>
2013-11-25 23:35:38 +0100

    Dynamic Class Loader management

--- src/main/resources/jvm_launcher.cpp
+++ src/main/resources/jvm_launcher.cpp
@@ -5,6 +5,7 @@
  *      Author: Loic Oudot
  */
 #include "jvm_launcher.h"
+#include "java4cpp_runtime.h"
 #include <fstream>
 #include <sstream>
 #include <vector>
@@ -32,7 +33,6 @@ JvmOptions _classPath;
 typedef jint (WINAPI *JNI_GetCreatedJavaVMs_t)(JavaVM **, jsize, jsize *);
 typedef jint (WINAPI *JNI_CreateJavaVM_t)(JavaVM **pvm, void **penv, void *args);
 
-
 void jw_setJrePath(const char* jrePath)
 {
    _jrePath = jrePath;
@@ -50,15 +50,15 @@ void jw_addOption(const char* option)
 
 void jw_addOptionsFromFile(const char* fileName)
 {
-   std::ifstream file( fileName );
-   if( file.good() )
+   std::ifstream file(fileName);
+   if (file.good())
    {
       std::string option;
-      while( std::getline(file, option) )
+      while (std::getline(file, option))
       {
-         if( !option.empty() && option.at(0) != '#')
+         if (!option.empty() && option.at(0) != '#')
          {
-            _options.push_back( option );
+            _options.push_back(option);
          }
       }
    }
@@ -71,10 +71,54 @@ void jw_clearClassPath()
 
 void jw_addClassPath(const char* path)
 {
-   if( std::find(_classPath.begin(), _classPath.end(), path) != _classPath.end() )
-      return;
+   if (_pVMJvm == NULL)
+   {
+      if (std::find(_classPath.begin(), _classPath.end(), path) != _classPath.end())
+         return;
 
-   _classPath.push_back(path);
+      _classPath.push_back(path);
+   }
+   else
+   {
+      JNIEnv *javaEnv = Java4CppRuntime::attachCurrentThread();
+
+      // File f = new File(path);
+      jclass fileCls = javaEnv->FindClass("java/io/File");
+      jmethodID fileCtor = javaEnv->GetMethodID(fileCls, "<init>", "(Ljava/lang/String;)V");
+      jstring jpath = javaEnv->NewStringUTF(path);
+      jobject f = javaEnv->NewObject(fileCls, fileCtor, jpath);
+      javaEnv->DeleteLocalRef(jpath);
+      Java4CppRuntime::handleJavaException(javaEnv);
+
+      // URI uri = f.toURI();
+      jmethodID toURI = javaEnv->GetMethodID(fileCls, "toURI", "()Ljava/net/URI;");
+      jobject uri = javaEnv->CallObjectMethod(f, toURI);
+      javaEnv->DeleteLocalRef(f);
+      javaEnv->DeleteLocalRef(fileCls);
+
+      // URL url = uri.toURL();
+      jclass uriCls = javaEnv->FindClass("java/net/URI");
+      jmethodID toURL = javaEnv->GetMethodID(uriCls, "toURL", "()Ljava/net/URL;");
+      jobject url = javaEnv->CallObjectMethod(uri, toURL);
+      javaEnv->DeleteLocalRef(uriCls);
+      javaEnv->DeleteLocalRef(uri);
+
+      // URLClassLoader urlClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+      jclass classLoaderCls = javaEnv->FindClass("java/lang/ClassLoader");
+      jmethodID getSystemClassLoader = javaEnv->GetStaticMethodID(classLoaderCls, "getSystemClassLoader", "()Ljava/lang/ClassLoader;");
+      jobject urlClassLoader = javaEnv->CallStaticObjectMethod(classLoaderCls, getSystemClassLoader);
+      javaEnv->DeleteLocalRef(classLoaderCls);
+      Java4CppRuntime::handleJavaException(javaEnv);
+
+      // urlClassLoader.addURL(url)
+      jclass urlClassLoaderCls = javaEnv->FindClass("java/net/URLClassLoader");
+      jmethodID addURL = javaEnv->GetMethodID(urlClassLoaderCls, "addURL", "(Ljava/net/URL;)V");
+      javaEnv->CallVoidMethod(urlClassLoader, addURL, url);
+      javaEnv->DeleteLocalRef(urlClassLoaderCls);
+      javaEnv->DeleteLocalRef(urlClassLoader);
+      javaEnv->DeleteLocalRef(url);
+      Java4CppRuntime::handleJavaException(javaEnv);
+   }
 }
 
 void setClassPath()
@@ -82,9 +126,9 @@ void setClassPath()
    std::string classPath = CLASS_PATH;
 
    // find classpath in options
-   for(JvmOptions::iterator it = _options.begin(); it != _options.end(); ++it)
+   for (JvmOptions::iterator it = _options.begin(); it != _options.end(); ++it)
    {
-      if( it->find(CLASS_PATH) != std::string::npos )
+      if (it->find(CLASS_PATH) != std::string::npos)
       {
          classPath = *it;
          _options.erase(it);
@@ -92,11 +136,11 @@ void setClassPath()
       }
    }
 
-   for(JvmOptions::iterator it = _classPath.begin(); it != _classPath.end(); ++it)
+   for (JvmOptions::iterator it = _classPath.begin(); it != _classPath.end(); ++it)
    {
-      if( classPath.find(*it) == std::string::npos )
+      if (classPath.find(*it) == std::string::npos)
       {
-         if( classPath != CLASS_PATH )
+         if (classPath != CLASS_PATH)
             classPath += ";";
          classPath += *it;
       }
@@ -107,7 +151,7 @@ void setClassPath()
 
 void initJVM()
 {
-   if( _pVMJvm != NULL )
+   if (_pVMJvm != NULL)
       return;
 
    JNIEnv* pVMEnv = NULL;
@@ -116,56 +160,57 @@ void initJVM()
 #else
    void *handle = dlopen(_jrePath.c_str(), RTLD_NOW);
 #endif
-   	if( handle == NULL )
-		throw std::runtime_error("initJVM: unable to load libjvm.");
+   if (handle == NULL)
+      throw std::runtime_error("initJVM: unable to load libjvm.");
 
-	JNI_GetCreatedJavaVMs_t JNI_GetCreatedJavaVMs = (JNI_GetCreatedJavaVMs_t)LoadSym(handle, "JNI_GetCreatedJavaVMs");
-	if( JNI_GetCreatedJavaVMs == NULL )
-		JNI_GetCreatedJavaVMs = (JNI_GetCreatedJavaVMs_t)LoadSym(handle, "JNI_GetCreatedJavaVMs_Impl");
-	if( JNI_GetCreatedJavaVMs == NULL )
-		throw std::runtime_error("initJVM: unable to find method JNI_GetCreatedJavaVMs.");
+   JNI_GetCreatedJavaVMs_t JNI_GetCreatedJavaVMs = (JNI_GetCreatedJavaVMs_t) LoadSym(handle, "JNI_GetCreatedJavaVMs");
+   if (JNI_GetCreatedJavaVMs == NULL)
+      JNI_GetCreatedJavaVMs = (JNI_GetCreatedJavaVMs_t) LoadSym(handle, "JNI_GetCreatedJavaVMs_Impl");
+   if (JNI_GetCreatedJavaVMs == NULL)
+      throw std::runtime_error("initJVM: unable to find method JNI_GetCreatedJavaVMs.");
 
-	// try to get an already created JVM
-	jsize nbVMs = 0;
-	JNI_GetCreatedJavaVMs(&_pVMJvm, 1, &nbVMs);
-	if( nbVMs == 0 )
-	{
-		 // Else : create a new JVM
-		setClassPath();
-		jint nbOptions = (jint)_options.size();
-		JavaVMOption *aOptions = new JavaVMOption[nbOptions];
-		for( jint i = 0; i < nbOptions; ++i )
-			aOptions[i].optionString = (char*)_options[i].c_str();
+   // try to get an already created JVM
+   jsize nbVMs = 0;
+   JNI_GetCreatedJavaVMs(&_pVMJvm, 1, &nbVMs);
+   if (nbVMs == 0)
+   {
+      // Else : create a new JVM
+      setClassPath();
+      jint nbOptions = (jint) _options.size();
+      JavaVMOption *aOptions = new JavaVMOption[nbOptions];
+      for (jint i = 0; i < nbOptions; ++i)
+         aOptions[i].optionString = (char*) _options[i].c_str();
 
-		JavaVMInitArgs oVMArgs;
-		memset(&oVMArgs, 0, sizeof(oVMArgs));
-		oVMArgs.version = JNI_VERSION_1_6;
-		oVMArgs.nOptions = nbOptions;
-		oVMArgs.options = aOptions;
+      JavaVMInitArgs oVMArgs;
+      memset(&oVMArgs, 0, sizeof(oVMArgs));
+      oVMArgs.version = JNI_VERSION_1_6;
+      oVMArgs.nOptions = nbOptions;
+      oVMArgs.options = aOptions;
 
-		JNI_CreateJavaVM_t JNI_CreateJavaVM = (JNI_CreateJavaVM_t)LoadSym(handle, "JNI_CreateJavaVM");
-		if( JNI_CreateJavaVM == NULL )
-			JNI_CreateJavaVM = (JNI_CreateJavaVM_t)LoadSym(handle, "JNI_CreateJavaVM_Impl");
-		if( JNI_CreateJavaVM == NULL )
-			throw std::runtime_error("initJVM: unable to find method JNI_CreateJavaVM.");
+      JNI_CreateJavaVM_t JNI_CreateJavaVM = (JNI_CreateJavaVM_t) LoadSym(handle, "JNI_CreateJavaVM");
+      if (JNI_CreateJavaVM == NULL)
+         JNI_CreateJavaVM = (JNI_CreateJavaVM_t) LoadSym(handle, "JNI_CreateJavaVM_Impl");
+      if (JNI_CreateJavaVM == NULL)
+         throw std::runtime_error("initJVM: unable to find method JNI_CreateJavaVM.");
 
-		jint result = JNI_CreateJavaVM(&_pVMJvm, (void**)&pVMEnv, (void*)&oVMArgs);
-		if( result != 0 ) {
-			std::ostringstream oss;
-			oss << "initJVM: can't create new JVM (" << result << ").";
-			 throw std::runtime_error(oss.str());
-		}
+      jint result = JNI_CreateJavaVM(&_pVMJvm, (void**) &pVMEnv, (void*) &oVMArgs);
+      if (result != 0)
+      {
+         std::ostringstream oss;
+         oss << "initJVM: can't create new JVM (" << result << ").";
+         throw std::runtime_error(oss.str());
+      }
 
-		delete[] aOptions;
-	}
+      delete[] aOptions;
+   }
 }
 
 JavaVM* getJVM()
 {
-   if( _pVMJvm == NULL )
+   if (_pVMJvm == NULL)
       initJVM();
 
-   if( _pVMJvm == NULL )
+   if (_pVMJvm == NULL)
       throw std::runtime_error("getJVM: no JVM.");
 
    return _pVMJvm;
--- src/main/resources/jvm_launcher.h
+++ src/main/resources/jvm_launcher.h
@@ -23,14 +23,14 @@ void jw_addOption(const char* option);
 void jw_addOptionsFromFile(const char* fileName);
 
 // Methods for managing the internal class path of the JVM (the -Djava.class.path option).
-// - Once the JVM is running (after getJVM()), these methods are no longer used.
+// - Once the JVM is running (after getJVM()), jw_addClassPath dynamically add class path to the system class loader.
 // - If a JVM already exist in the process, the internal class path is ignored.
 void jw_clearClassPath();
 void jw_addClassPath(const char* path);
 
 // Return the JVM instance of the process.
 // - If no JVM is running in the process, a new one is instanciated by passing the internal list of options and class path defined above.
-// - If a JVM already exist in the process, this instance is returned and the internal list of options and class path is ignored.
+// - If a JVM already exist in the process, the instance is returned and the internal list of options and class path are ignored.
 JavaVM* getJVM();
 
 #endif
